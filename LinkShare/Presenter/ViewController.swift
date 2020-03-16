b2ece39f4b0380feb89f4795481d7a12be48fa02
mcolja <colja.martin@gmail.com>
2020-03-16 18:44:56 +0100

    test commit

--- LinkShare/Presenter/ViewController.swift
+++ LinkShare/Presenter/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loginButton.layer.cornerRadius = loginButton.frame.height * K.cornerRadius
         registerButton.layer.cornerRadius = registerButton.frame.height * K.cornerRadius
+        print("have to test something")
     }
     
     override func viewWillAppear(_ animated: Bool) {
